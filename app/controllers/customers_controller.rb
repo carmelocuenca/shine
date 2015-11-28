2cee86d84ce8b9be0696d25cbb78e19b5d57bc1f
Carmelo Cuenca <carmelocuenca@hotmail.com>
2015-11-28 20:05:08 +0000

    naive-search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,12 @@
 class CustomersController < ApplicationController
   def index
-  	@customers = Customer.all.limit(10)
+  	if params[:keywords].present?
+      customer_search_term = CustomerSearchTerm.new( params[:keywords] )
+      @customers = Customer.where(
+      	customer_search_term.where_clause,
+      	customer_search_term.where_args).order(customer_search_term.order)
+  	else
+  	  @customers = []
+  	end
   end
 end
